af92341950bc6d353f8d14b3e4ac22a8cfd5a4a5
Abhijeet Jadhav <150771079+AbhijeetAJ-18@users.noreply.github.com>
2023-11-26 09:15:06 +0530

    Add files via upload

---
+++ constants/index.ts
@@ -0,0 +1,137 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  // {
+  //   name: "TypeScript",
+  //   Image: "/ts.png",
+  //   width: 80,
+  //   height: 80,
+  // },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  // {
+  //   name: "Stripe Payment",
+  //   Image: "/stripe.webp",
+  //   width: 80,
+  //   height: 80,
+  // },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  // {
+  //   name: "Mongo db",
+  //   Image: "/mongodb.png",
+  //   width: 40,
+  //   height: 40,
+  // },
+];
+
+export const Socials = [
+  {
+    name: "Discord",
+    src: "/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    src: "/facebook.svg",
+  },
+  {
+    name: "Instagram",
+    src: "/discord.svg",
+  },
+];
+export const Projects = [
+  {
+    title: "Modern Nextjs Website",
+    text: "Next. js is a powerful and flexible framework that can be used to build a wide range of web applications.",
+    src: "/NextWebsite.png",
+  },
+  {
+    title: "Space Themed Website",
+    text: "For the wise man looks into space and he knows there is no limited dimensions.",
+    src: "/SpaceWebsite.png",
+  },
+  {
+    title: "Modern Nextjs Portfolio",
+    text: "If you are looking for a framework that can help you build high-performance, SEO-friendly web applications, then Next.",
+    src: "/project.png",
+  },
+  {
+    title: "Matrix themed Website",
+    text: "everyone has the individual responsibility to make the choice between the real world and an artificial world. ",
+    src: "/Matrix.png",
+  },
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
